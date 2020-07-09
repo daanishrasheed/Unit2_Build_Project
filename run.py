81b3eeb303e43b4da4c5dd447948bc35bdf8b1c2
Daanish Rasheed <daanishrasheed@gmail.com>
2020-07-09 16:40:01 -0400

    added name

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span('Your Name', className='mr-2'), 
+                    html.Span('Daanish Rasheed', className='mr-2'), 
                     html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
                     html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
                     html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
